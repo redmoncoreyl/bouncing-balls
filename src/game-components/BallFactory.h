6ff17ce5942bb01c91c67e0245bb7fe27b95928e
Corey Redmon <redmoncoreyl@gmail.com>
2023-07-25 17:29:41 -0400

    adds state member

--- src/game-components/BallFactory.h
+++ src/game-components/BallFactory.h
@@ -1,6 +1,15 @@
 #ifndef BALL_FACTORY_H
 #define BALL_FACTORY_H
 
+class BallFactory {
+public:
+	enum class State { STAND_BY, BUILDING_BALL, BALL_READY };
 
+private: 
+	State state;
+	
+public: 
+	
+};
 
 #endif
