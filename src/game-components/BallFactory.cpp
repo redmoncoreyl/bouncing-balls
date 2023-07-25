032ba6298705fe1a435ea5a298e27bd344394478
Corey Redmon <redmoncoreyl@gmail.com>
2023-07-25 17:46:07 -0400

    implements singleton pattern

--- src/game-components/BallFactory.cpp
+++ src/game-components/BallFactory.cpp
@@ -1,1 +1,9 @@
 #include "BallFactory.h"
+
+BallFactory::BallFactory() : state(State::STAND_BY) {}
+
+BallFactory BallFactory::instance = BallFactory();
+
+BallFactory& BallFactory::getInstance() {
+	return instance;
+}
