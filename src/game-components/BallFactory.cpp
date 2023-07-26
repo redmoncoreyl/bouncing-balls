91ad300ba39c6a55ab5faa4efdc5b7b5d8d6efb2
Corey Redmon <redmoncoreyl@gmail.com>
2023-07-25 22:26:47 -0400

    adds updateSelf function and state transitions

--- src/game-components/BallFactory.cpp
+++ src/game-components/BallFactory.cpp
@@ -7,3 +7,9 @@ BallFactory BallFactory::instance = BallFactory();
 BallFactory& BallFactory::getInstance() {
 	return instance;
 }
+
+std::optional<Ball> BallFactory::updateSelf(float elapsedTimeSeconds, olc::HWButton leftMouseButton) {
+	if (state == State::STAND_BY && leftMouseButton.bPressed) state = State::BUILDING_BALL;
+	else if (state == State::BUILDING_BALL && leftMouseButton.bPressed) state = State::BALL_READY;
+	else if (state == State::BALL_READY && leftMouseButton.bReleased) state = State::STAND_BY;
+}
--- src/game-components/BallFactory.h
+++ src/game-components/BallFactory.h
@@ -1,6 +1,11 @@
 #ifndef BALL_FACTORY_H
 #define BALL_FACTORY_H
 
+#include <optional>
+
+#include "../game-engine/olcPixelGameEngine.h"
+#include "Ball.h"
+
 class BallFactory {
 public:
 	enum class State { STAND_BY, BUILDING_BALL, BALL_READY };
@@ -14,6 +19,8 @@ private:
 
 public:
 	static BallFactory& getInstance();
+
+	std::optional<Ball> updateSelf(float elapsedTimeSeconds, olc::HWButton leftMouseButton);
 	
 };
 
