6850d4adbe39b13dc65d50199338b16579adf32f
Corey Redmon <redmoncoreyl@gmail.com>
2023-07-25 22:56:45 -0400

    adds always return nullopt for now

--- src/game-components/BallFactory.cpp
+++ src/game-components/BallFactory.cpp
@@ -12,4 +12,6 @@ std::optional<Ball> BallFactory::updateSelf(float elapsedTimeSeconds, olc::HWBut
 	if (state == State::STAND_BY && leftMouseButton.bPressed) state = State::BUILDING_BALL;
 	else if (state == State::BUILDING_BALL && leftMouseButton.bPressed) state = State::BALL_READY;
 	else if (state == State::BALL_READY && leftMouseButton.bReleased) state = State::STAND_BY;
+	
+	return std::nullopt;
 }
